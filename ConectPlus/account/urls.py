4c995f0dbe35d242f6c7b2ddf54dea38d8842243
litesh1998 <litesh1998@gmail.com>
2021-03-05 20:52:00 +0530

    Added User register Functionality

--- ConectPlus/account/urls.py
+++ ConectPlus/account/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # path('login/',views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
+    path('register', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout_acc'),
     path('password_change/',
@@ -26,4 +27,5 @@ urlpatterns = [
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+     
 ]
--- ConectPlus/account/views.py
+++ ConectPlus/account/views.py
@@ -3,16 +3,18 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
 
 def user_login(request):
     if request.method == 'POST':
-        form=LoginForm(request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
-            cd=form.cleaned_data
-            user=authenticate(request, username=cd['username'],
-                            password=cd['password'])
-            
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'],
+                                password=cd['password'])
+
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -22,12 +24,35 @@ def user_login(request):
             else:
                 return HttpResponse('Invalid Login')
     else:
-        form=LoginForm()
-    return render(request, 'account/login.html', {'form':form})
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
 
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html',
-                        {
-                            'section':'dashboard'
-                        })
+                  {
+                      'section': 'dashboard'
+                  })
+
+
+def register(request):
+    
+
+
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password1'])
+            # Save the User object
+            new_user.save()
+            return render(request,
+                        'account/register_done.html',
+                        {'new_user': new_user})
+    else:
+        user_form = UserCreationForm()
+    return render(request,'account/register.html',{'user_form': user_form})
